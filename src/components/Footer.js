ba967c8b9081559bf7bda4f0277b0babcb52b57a
Your Name <nejiba.chaoua.issat@gmail.com>
2021-03-17 18:35:04 +0100

    footer-issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,16 +12,16 @@ const Footer = () => {
         <Row>
         <Col  md={4} > <h style={{color:"#7DAF3F",fontWeight:"bold"}}>Mon</h><h style={{color:"#4D302B",fontWeight:"bold"}}>Extérieur.fr</h> </Col> 
         <Col>
-          <ul style={{font: "normal normal bold 18px/23px PT Sans"}}>liens utiles</ul> 
-        <ul>Configurateu</ul>
-        <ul>Notre magasin</ul>
-        <ul>Notice & installation</ul>
-        <ul>Livraison & transport</ul>
+          <h style={{font: "normal normal bold 18px/23px PT Sans"}}>liens utiles</h> <br></br>
+        <h>Configurateu</h><br></br>
+        <h>Notre magasin</h><br></br>
+        <h>Notice & installation</h><br></br>
+        <h>Livraison & transport</h><br></br>
        </Col>
        <Col>
-         <ul  style={{font: "normal normal bold 18px/23px PT Sans"}}>Nous contacter </ul> 
-        <ul>3 rue du champ de verger Allonnes, Pays de la Loire, France</ul>
-        <ul>Télephone:+33 2 43 57 00 87</ul>
+         <h  style={{font: "normal normal bold 18px/23px PT Sans"}}>Nous contacter </h> <br></br>
+        <h>3 rue du champ de verger Allonnes, Pays de la Loire, France</h><br></br>
+        <h>Télephone:+33 2 43 57 00 87</h><br></br>
        
        </Col>
       </Row>
